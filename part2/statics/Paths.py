56f70b93f17813cf617a4602dd79a0472bf5315f
sadra-f <sadrafouladi@gmail.com>
2023-05-19 16:19:01 +0330

    added paths to datset files

---
+++ part2/statics/Paths.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+#==============================================================================
+
+DATASET_LOCAL_PATH_STR = 'data/cran.all.1400'
+DATASET_PATH_STR = 'part2/data/cran.all.1400'
+
+QUERY_LOCAL_PATH_STR = 'data/cran.qry'
+QUERY_PATH_STR = 'part2/data/cran.qry'
+
+RELEVANCE_LOCAL_PATH_STR = 'data/cranqrel'
+RELEVANCE_PATH_STR = 'part2/data/cranqrel'
+
+#==============================================================================
+
+DATASET_PATH = Path(DATASET_LOCAL_PATH_STR)
+
+DATASET_PATH = Path(QUERY_LOCAL_PATH_STR)
+
+DATASET_PATH = Path(RELEVANCE_LOCAL_PATH_STR)
