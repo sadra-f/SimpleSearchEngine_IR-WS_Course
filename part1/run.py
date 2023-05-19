06edcb210469034d599346b8dba133bb8b3250f6
sadra-f <sadrafouladi@gmail.com>
2023-05-19 16:18:30 +0330

    moved part1 code previously in main.py, to part1/run.py

---
+++ part1/run.py
@@ -0,0 +1,13 @@
+from  part1.InvertedIndex import InvertedIndex as IX
+from part1.SimpleSearch import SimpleSearch as Searcher
+import os
+
+def main():
+    indexer = IX(os.getcwd())
+    print(indexer.inverted_index)
+    srch = Searcher(indexer.inverted_index, set([i for i in range(0,len(indexer.docs))]))
+    print(srch.query("sadra&!helps"))
+
+
+if __name__ == '__main__':
+    main()
