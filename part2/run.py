bba28cd2fa1cd818b0b03e4ff87addebaf94ce26
sadra-f <sadrafouladi@gmail.com>
2023-05-25 15:52:49 +0330

    added code to test tfidf

--- part2/run.py
+++ part2/run.py
@@ -1,7 +1,12 @@
 from IO.Read import Reader
-
+from vectorize.TFIDF import TFIDF
 
 rds = Reader()
 
-rds.read_dataset()
-rds.read_queries()
+inp = rds.read_dataset()
+test = rds.read_queries()
+
+model = TFIDF([inp[i].text for i in range(len(inp))], [test[i].text for i in range(len(test))])
+
+print("Model")
+
