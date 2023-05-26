89ce1cbe3340207a281f35a5af52b678541bacd9
sadra-f <sadrafouladi@gmail.com>
2023-05-26 18:49:13 +0330

    added class to represent relevance result type

---
+++ part2/models/RRR.py
@@ -0,0 +1,7 @@
+
+class RelevanceResultRecord:
+
+    def __init__(self, query_id, dataset_id, relevance):
+        self.query_id = query_id
+        self.dataset_id = dataset_id
+        self.relevance = relevance
